fb82c5ed3acab6a1a6e0055f7e9abfa3c164f213
afei <dxlinfo@gmail.com>
2014-05-05 16:40:48 +0800

    js

---
+++ javascript/Option四个参数意思.js
@@ -0,0 +1,4 @@
+
+new Option(text,value,defaultselected,selected)
+new Option(文本,值,默认选中的选项,选中的选项) 
+
---
+++ javascript/获取扩展名.js
@@ -0,0 +1,4 @@
+
+var ext = filepath.split('.').pop().toLowerCase();
+
+//就是按照 . 分割成数组，并取出数组最后一个元素。
